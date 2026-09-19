08d2f920e39bfe88490ddf2b29f9e4d302fe7764
unknown <kenneth.gulmatico@hcdc.edu.ph>
2026-09-19 14:49:39 +0800

    Include .onrender.com in default ALLOWED_HOSTS list

--- config/settings.py
+++ config/settings.py
@@ -32,10 +32,9 @@ DEBUG = os.getenv('DEBUG', 'True').lower() in {'1', 'true', 'yes'}
 
 ALLOWED_HOSTS = [
     host.strip()
-    for host in os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,testserver').split(',')
+    for host in os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,testserver,.onrender.com').split(',')
     if host.strip()
 ]
-
 
 # Application definition
 
